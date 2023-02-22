04793c0899c2f8d415b4663435ba0f6b82320961
mona <aj.ajay.sharma@gmail.com>
2023-02-22 07:32:53 +0530

    adding job to the chain

---
+++ JobChain_02.java
@@ -0,0 +1,11 @@
+
+public class JobChain_02{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World,this is child job chaining code");
+
+	}
+
+}
